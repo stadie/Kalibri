ca090007329005e8190d27b619a8a1895a6c3a79
stadie <>
2008-12-17 09:38:36 +0000

    new code for root finding, do not scale error in derivative calculation

--- JetTruthEvent.cc
+++ JetTruthEvent.cc
@@ -2,7 +2,7 @@
 //    Class for all events with one jet and truth informatio
 //
 //    first version: Hartmut Stadie 2008/12/14
-//    $Id: EventReader.h,v 1.1 2008/12/12 13:43:15 stadie Exp $
+//    $Id: JetTruthEvent.cc,v 1.1 2008/12/16 15:21:27 stadie Exp $
 //   
 
 #include "JetTruthEvent.h"
@@ -25,11 +25,20 @@ double JetTruthEvent::chi2_fast(double * temp_derivative1, double * temp_derivat
     int i = 0;
     double x1 = truth * 0.5,x2 = 2 * truth;
     double y1 = truth - jet->correctedEt(x1), y2 = truth - jet->correctedEt(x2);
-    //std::cout << y1 << "," << y2 << '\n';
-    assert(y2 < 0);
-    assert(y1 > 0);
+    //bag the root
+    while(((y2 > 0)||(y1 < 0))&& i < 5) {
+      if(y2 > 0) x2 *= 2;
+      else x1 /= 2;
+      ++i;
+    }
     double xm,ym;
+    if(i == 5) {
+      i = 99;
+      x1 = x2 = truth;
+    }
+    else i = 0;
     while(std::abs(x1 - x2) > 0.1) { 
+      if(i < 20) break;
       xm  = 0.5 * (x1 + x2);
       ym = jet->correctedEt(xm);
       if(ym < 0) {
@@ -40,7 +49,6 @@ double JetTruthEvent::chi2_fast(double * temp_derivative1, double * temp_derivat
 	y1 = ym;
       }
       ++i;
-      if(i < 20) break;
     }
     //std::cout << "I:" << i << ", " <<  y1 << "," << y2 << '\n';
     expectedEt = 0.5 * (x1 + x2);
@@ -50,9 +58,11 @@ double JetTruthEvent::chi2_fast(double * temp_derivative1, double * temp_derivat
   double err2inv = jet->Error();
   err2inv *= err2inv;
   err2inv = 1/err2inv;
-  double s = jet->correctedEt(expectedEt)/expectedEt;
-  double et = s * jet->Et();
-  double s2inv =1/s;
+  double et = jet->correctedEt(expectedEt);
+  double hadf = jet->HadEt()/jet->Et();
+  double s = et/expectedEt;
+  et = s * jet->Et();
+  double s2inv =hadf/s;
   s2inv *= s2inv;
   double chi2 = et - truth;
   chi2 *= chi2 * err2inv * s2inv;
@@ -62,16 +72,18 @@ double JetTruthEvent::chi2_fast(double * temp_derivative1, double * temp_derivat
   double et1,et2,temp1,temp2;
   for(int i = 0 ; i < npar ; ++i) {
     int parid = jet->varyPar(i,epsilon,expectedEt,et2,et1);
-    et1 = et1/expectedEt * jet->Et();
-    et2 = et2/expectedEt * jet->Et();
+    s = et1/expectedEt;
+    et1 = s * jet->Et();
     temp1 = et1 - truth; 
-    s2inv = jet->Et()/et1;
-    s2inv *= s2inv;
+    //s2inv = hadf/s;
+    //s2inv *= s2inv;
     temp1 *= temp1 * err2inv * s2inv;
     temp1 = weight * TData::ScaleResidual(temp1);
+    s = et2/expectedEt;
+    et2 = s * jet->Et();
     temp2 = et2 - truth;
-    s2inv = jet->Et()/et2;
-    s2inv *= s2inv;
+    //s2inv = hadf/s;
+    //s2inv *= s2inv;
     temp2 *= temp2 * err2inv * s2inv;
     temp2 = weight * TData::ScaleResidual(temp2);
     temp_derivative1[parid] += (temp2 - temp1); // for 1st derivative
