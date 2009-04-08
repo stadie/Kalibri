6b664e1f6a13c37c5cddf5020084ca75bf9c7aca
mschrode <>
2009-04-08 16:39:54 +0000

    Some more documentation

--- caliber.h
+++ caliber.h
@@ -1,8 +1,57 @@
-//
-// Original Author:  Christian Autermann
-//         Created:  Wed Jul 18 13:54:50 CEST 2007
-// $Id: caliber.h,v 1.39 2009/01/18 13:13:39 stadie Exp $
-//
+//!  \mainpage
+//!
+//!  \section label_sec_intro Introduction
+//!  Package for data driven calibration using a global fit (see also the related
+//!  <A HREF="https://twiki.cern.ch/twiki/bin/view/CMS/Calibration">
+//!  Twiki Page</A>).
+//!  \image html kalibri_workflow.jpg
+//!
+//!  \section label_sec_src Source Code
+//!  The source code can be found
+//!  <A HREF="http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/UserCode/Bromo/Calibration/CalibCore/">here</A>.
+//!
+//!  \section label_sec_relinfo Related information
+//!  - C. Autermann:
+//!    <A HREF="http://indico.cern.ch/getFile.py/access?contribId=7&resId=0&materialId=slides&confId=22705">
+//!    A global fit approach to HCAL/jet calibration</A>,
+//!    JetMET Meeting, 18th October 2007
+//!  - R. Wolf:
+//!    <A HREF="http://indico.cern.ch/getFile.py/access?contribId=3&resId=0&materialId=slides&confId=29582">
+//!    Data-Driven Calorimeter Calibration Exploiting a Global-Fit Ansatz</A>,
+//!    JetMET Meeting, 26th February 2008
+//!  - T. Schum:
+//!    <A HREF="https://indico.desy.de/getFile.py/access?contribId=1&amp;resId=0&amp;materialId=slides&amp;confId=627">
+//!    HCAL Calibration using a Global Fit Ansatz</A>,
+//!    Hamburg CMS Meeting, 12th March 2008
+//!  - S. Naumann-Emme: Top as a Calibration Tool,
+//!    2nd "Physics at the Terascale" Workshop, 27th November 2008
+//!  - H. Stadie:
+//!    <A HREF="https://indico.desy.de/getFile.py/access?contribId=1&amp;resId=0&amp;materialId=slides&amp;confId=1683">
+//!    CMS Calorimeter and Jet Calibration</A>,
+//!    CMS Hamburg Meeting, 28th January 2009
+//!  - M. Schr&ouml;der:
+//!    <A HREF="https://indico.desy.de/getFile.py/access?contribId=2&amp;resId=0&amp;materialId=slides&amp;confId=1683">
+//!    Conceptual Studies for a Jet Energy Correction</A>,
+//!    CMS Hamburg Meeting, 28th January 2009
+//!  - S. Naumann-Emme:
+//!    <A HREF="https://indico.desy.de/getFile.py/access?contribId=3&amp;resId=0&amp;materialId=slides&amp;confId=1683">
+//!    Jet Energy Corrections from Top Quark Decays</A>,
+//!    CMS Hamburg Meeting, 28th January 2009
+//!
+//!  \section label_sec_authors Authors
+//!  - Christian Autermann
+//!  - Ulla Gebbert
+//!  - Robert Klanner
+//!  - Bj&ouml;rn Kolodzey
+//!  - Sebastian Naumann-Emme
+//!  - Christian Sander
+//!  - Matthias Schr&ouml;der
+//!  - Torben Schum
+//!  - Hartmut Stadie
+//!  - Jan Thomsen
+//!  - Roger Wolf
+ 
+
 #ifndef caliber_h
 #define caliber_h
 
@@ -14,6 +63,10 @@ class TControlPlots;
 class TData;
 class TMeasurement;
 
+
+//!  \author Christian Autermann
+//!  \date Wed Jul 18 13:54:50 CEST 2007
+//!  $Id: caliber.h,v 1.40 2009/02/01 16:38:19 stadie Exp $
 class TCaliber {
 public :
   TCaliber(const std::string& f)
