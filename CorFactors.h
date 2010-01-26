cee30c2864a1af8d331f2d5c2a7ed272255d4d40
stadie <>
2010-01-26 16:38:17 +0000

    do not apply L1/ZSP in correctToL2

--- CorFactors.h
+++ CorFactors.h
@@ -1,6 +1,6 @@
 //!  \brief   Container class for jet correction factors
 //
-//    $Id: CorFactors.h,v 1.1 2009/11/25 13:07:45 stadie Exp $
+//    $Id: CorFactors.h,v 1.2 2009/11/26 10:27:48 stadie Exp $
 //   
 #ifndef CORFACTORS_H
 #define CORFACTORS_H
@@ -19,7 +19,7 @@ class CorFactors
   double getJPT() const { return jpt_; }   //!< Return Jet+Track correction factor
   double getL2L3() const { return l2_*l3_; }   //!< Return product of L2 and L3 correction factors
   double getJPTL2L3() const { return jptL2L3_; }   //!< Return product of L2 and L3 correction factors for Jet+Track
-  double getToL2() const { return l1_*l2_; }         //!< Return factor needed to get L2 corrected from raw jets: L1*L2
+  double getToL2() const { return l2_; }         //!< Return factor needed to get L2 corrected from raw jets: L1*L2
   double getToL3() const { return getToL2()*l3_; }   //!< Return factor needed to get L3 corrected from raw jets: L1*L2*L3
   double getToL4() const { return getToL3()*l4_; }   //!< Return factor needed to get L4 corrected from raw jets: L1*L2*L3*L4
   double getToL5() const { return getToL4()*l5_; }   //!< Return factor needed to get L5 corrected from raw jets: L1*L2*L3*L4*L5
