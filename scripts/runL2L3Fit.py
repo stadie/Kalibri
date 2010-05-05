b545bae7246c2aaf70544038464f6d82b2bf80c6
stadie <>
2010-05-05 14:15:24 +0000

    script to submit L2L3 fits

---
+++ scripts/runL2L3Fit.py
@@ -0,0 +1,202 @@
+#! /usr/bin/python
+
+import os
+
+config="""
+#
+# Configuration File for the Calibration Program
+# Hamburg, 2007/08/15
+#
+
+#---------------------------------------------------------------------------------
+#   Mode
+#---------------------------------------------------------------------------------
+# Running mode: Calibration (0), Jet smearing (1)
+Mode = 0
+
+
+#---------------------------------------------------------------------------------
+#Fit method: LVMini=1, use existing calibration=2, write start values=3
+Fit method = 1
+#Parametrization
+Parametrization Class = L2L3JetParametrization
+
+#Error Parametrization
+tower error parametrization = const
+jet error parametrization   = jet et
+#jet error parametrization   = toy
+
+start values = 1.0
+jet start values        = 1. 0. 0. 0. 0. 0.
+global jet start values = 0.998 4.997 3.084 2.048
+
+# Scaling of residuals: 0 - none, 1 - with Cauchy-Function, 2 - with Huber-Function
+Residual Scaling Scheme    = 111 #   221 : default
+Outlier Cut on Chi2        = 1000.0 # Applied before each iteration with no scaling
+
+BFGS derivative step     = 1e-07
+BFGS mvec                = 6
+BFGS niter               = 100
+BFGS eps                 = 1e-02
+BFGS 1st wolfe parameter = 1.E-4
+BFGS 2nd wolfe parameter = 0.9
+BFGS print derivatives   = false
+
+
+#---------------------------------------------------------------------------------
+#   Geometry / Binning for fitting
+#---------------------------------------------------------------------------------
+maximum eta twr used  = 82
+granularity in eta    = 2   # allowed values are: 1,3,5,11,21,41 (*2)
+granularity in phi    = 1   # allowed values are: 1,2,3,6,9,18,36,72
+symmetry in eta       = false
+
+jet granularity in eta = 82 #  allowed values are: 1,3,5,11,21,41 (*2)
+jet granularity in phi = 1 #   1 : default
+
+track granularity in eta = 1
+track granularity in phi = 1
+
+
+#---------------------------------------------------------------------------------
+#   Analyses (GammaJet, TrackTower, ... )
+#---------------------------------------------------------------------------------
+Et cut on jet              = 0.0
+Et cut on gamma            = 0.0
+Et cut on Z                = 20.0
+Et cut on tower            = 0.0
+Et cut on cluster          = 0.0
+Et cut on track            = 0.0
+Et cut on n+1 Jet          = 0.0
+Eta cut on jet             = 6.0
+Relative Rest Jet Cut      = 0.2      #NonLeadingJetsEt / PhotonEt
+Min had fraction           = -0.05    #Default: 0.07
+Max had fraction           = 1.05    #Default: 0.95
+Et genJet min              = 0.0
+Et genJet max              = 7000.0
+DeltaR cut on jet matching = 0.25
+
+#---------------------------------------------------------------------------------
+#   Input / Output
+#---------------------------------------------------------------------------------
+
+#jet correction source = JetMETCor
+#jet correction name   = Spring10_AK5TRK
+Default Tree Name      = CalibTree
+
+# List of input files:
+Gamma-Jet tree         = GammaJetTree
+Z-Jet tree             = ZJetTree
+Track-Tower tree       = TrackTowerTree
+Track-Cluster tree     = TrackClusterTree
+Di-Jet tree            = DiJetTree
+Tri-Jet tree           = TriJetTree
+Top tree               = TopTree
+
+#Di-Jet input file = input/dijetlistspring10Track
+#Di-Jet input file =  input/toy_dijet_const_5-500_uniform.root
+
+#Top input file = /scratch/current/cms/user/stadie/Top_Madgraph.root
+#Z-Jet input file = /scratch/current/cms/user/stadie/ZJet_Track_230_300_rereco.root; /scratch/current/cms/user/stadie/ZJet_Track_300_INF_rereco.root; 
+
+# -1: use all events, 1000: run over 1000 events
+use Gamma-Jet events     = 0
+use Track-Tower events   = 0
+use Track-Cluster events = 0
+#use Di-Jet events        = -1
+use Tri-Jet events       = 0
+use Z-Jet events         = 0
+use Top events           = 0
+
+Gamma-Jet data class     = 1
+Z-Jet data class     = 3
+Di-Jet data class    = 11
+Top data class       = 1
+
+#Di-Jet prescale = 1000
+#-----------------------------------------------------------------
+#  Control plots
+#-----------------------------------------------------------------
+#  General parameters
+create plots                     = true
+plots output directory           = plots
+#plots format                      = pdf
+
+# JetTruthEvent plots
+create JetTruthEvent plots    =  true
+
+JetTruthEvent plots names =  MCTruthResponseVsGenJetPt; MCTruthResponseVsEta
+MCTruthResponseVsGenJetPt x variable        =  GenJetPt;  log
+MCTruthResponseVsGenJetPt x edges           =  30 10 3000
+MCTruthResponseVsGenJetPt y variable        =  GenJetResponse
+MCTruthResponseVsGenJetPt y edges           =  51 0 2.0 0.9 1.1 0.0 0.5
+MCTruthResponseVsGenJetPt bin variable      =  Eta
+MCTruthResponseVsGenJetPt bin edges         =  -5.0 -3.0 -1.3 1.3 3.0 5.0
+MCTruthResponseVsGenJetPt correction types  =  Uncorrected; L2L3; Kalibri
+MCTruthResponseVsGenJetPt profile types     =  GaussFitMean; GaussFitWidth
+#MCTruthResponseVsGenJetPt distributions    =  Uncorrected; L2L3; Kalibri
+MCTruthResponseVsGenJetPt legend label      =  L2L3:CMS default
+
+JetTruthEvent plots name 2              =  MCTruthResponseVsEta
+MCTruthResponseVsEta x variable         =  Eta
+MCTruthResponseVsEta x edges            =  20 -5 5
+MCTruthResponseVsEta y variable         =  GenJetResponse
+MCTruthResponseVsEta y edges            =  51 0 2.0 0.9 1.1 0.0 0.5
+MCTruthResponseVsEta bin variable       =  GenJetPt
+MCTruthResponseVsEta bin edges          =  10 50 100 500 2000
+MCTruthResponseVsEta correction types   =  Uncorrected; L2L3; Kalibri
+MCTruthResponseVsEta profile types      =  GaussFitMean; GaussFitWidth
+#MCTruthResponseVsEta distributions      =  Uncorrected; L2L3; Kalibri
+MCTruthResponseVsEta legend label       =  L2L3:CMS default
+
+"""
+
+jettype = "Calo"
+datadir = "/scratch/hh/current/cms/user/stadie/QCDFlat_Pt15to3000Spring10-START3X_V26_S09-v1C"
+nthreads = 4
+nevents = -1
+dirname = "L2L3fit"
+useconstraint = False
+
+print "fit L2L3 correction for "+jettype+" using "+datadir
+
+
+if os.path.exists(dirname):
+    os.system("rm -rf "+dirname+"/*")
+else:
+    os.system("mkdir "+dirname)
+    
+os.system("cp junk "+dirname+"/.")
+os.system("cd "+dirname+"; ln -s ../kalibriLogoSmall.gif ; cd -")
+
+os.system("ls "+datadir+"/*"+jettype+"*.root > "+dirname+"/dijetlist");
+fcfg = open(dirname+"/L2L3.cfg", "w")
+fcfg.write(config)
+fcfg.write("Di-Jet input file = dijetlist\n")
+fcfg.write("Output file       = Kalibri.txt\n");
+fcfg.write("Number of Threads = "+str(nthreads)+"\n")
+if(useconstraint):
+    fcfg.write("jet constraints =  5.0 10.0 0.0 1.2 1 10.0 15.0 0.0 1.2 1 15.0 20.0 0 1.2 1 20.0 25.0 0 1.2 1 25.0 30.0 0 1.2 1 30.0 40.0 0 1.2 1 40.0 50.0 0 1.2  1 50.0 60.0 0 1.2 1 60.0 70.0 0 1.2  1 70.0 80 0 1.2 1  80.0 90.0 0 1.2 1 90.0 100.0 0 1.2 1 100.0 120. 0 1.2 1 120 150 0 1.2 1 150 200 0 1.2 1 200 280 0 1.2 1 280 350 0 1.2 1 350 500 0 1.2 1 500 800 0 1.2 1 800 1400 0 1.2 1 1400 7000 0 1.2 1\n")
+else:
+    fcfg.write("fixed jet parameters = 7 1\n") 
+    
+fcfg.write("use Di-Jet events = "+str(nevents)+"\n")
+fcfg.close()
+
+
+fjob = open(dirname+"/fitL2L3.sh", "w")
+fjob.write("#! /bin/sh\n")
+fjob.write("#\n")
+fjob.write("#$ -V\n")
+fjob.write("#$ -pe  multicore "+str(nthreads)+"\n")
+fjob.write("#$ -R Y\n")
+fjob.write("#$ -l h_cpu=23:00:00\n")
+fjob.write("#$ -l h_vmem=5000M\n")
+fjob.write("cd "+os.getcwd()+"/"+dirname+"\n")
+fjob.write("./junk L2L3.cfg\n")
+fjob.close()
+
+qsubcmd = "qsub "+dirname+"/fitL2L3.sh"
+print "running "+qsubcmd
+os.system(qsubcmd)
+
