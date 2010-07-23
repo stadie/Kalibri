1ac114de61d56dd510fc1231c2eb6f96f2d7f588
stadie <>
2010-07-23 13:04:18 +0000

    add residual corrections

--- JetMETCorFactorsFactory.cc
+++ JetMETCorFactorsFactory.cc
@@ -1,19 +1,31 @@
 //
-//    $Id: JetMETCorFactorsFactory.cc,v 1.7 2010/04/29 13:29:41 stadie Exp $
+//    $Id: JetMETCorFactorsFactory.cc,v 1.8 2010/05/01 09:35:34 stadie Exp $
 //   
 #include "JetMETCorFactorsFactory.h"
 #include "CorFactors.h"
 #include "Jet.h"
 
 #include "JetMETObjects/interface/FactorizedJetCorrector.h"
+#include "JetMETObjects/interface/JetCorrectorParameters.h"
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 JetMETCorFactorsFactory::JetMETCorFactorsFactory(const std::string& name,
 						 const std::string& files)
   : CorFactorsFactory(name)
 {
-  cor_ = new FactorizedJetCorrector("L2Relative:L3Absolute",files);
+  //split files by ":"
+  std::vector<JetCorrectorParameters> vParam;
+  std::stringstream ss(files);
+  std::string file;
+  while(std::getline(ss,file,':')) {
+    vParam.push_back(JetCorrectorParameters(file));
+  }
+  cor_ = new FactorizedJetCorrector(vParam);
+  
   //std::cout << "created JetMETCorFactorsFactory: " << name << '\n';
 }
 
@@ -33,9 +45,13 @@ CorFactors* JetMETCorFactorsFactory::create(const Jet* j)
   
   std::vector<float> levels = cor_->getSubCorrections();
 
+  //std::cout << "eta:" << j->eta() << "  cor levels:" << levels.size() << " :";
+  //std::cout << levels[0] << ", " << levels[1];
+  //if(levels.size() == 3) std::cout << ", "<< levels[2] << '\n';
+  //else std::cout << levels[2] << '\n';
   return new CorFactors(1.0,
 			levels[0],
-			levels[1]/levels[0],
+			(levels.size() == 3) ? levels[2]/levels[0] : levels[1]/levels[0],
 			1.0,1.0,0.0,0.0);			
 }
 JetMETCorFactorsFactory::Register JetMETCorFactorsFactory::register_;
@@ -49,6 +65,9 @@ JetMETCorFactorsFactory::Register::Register()
   create("Spring10_AK5PF","JetMETObjects/data/Spring10_L2Relative_AK5PF.txt:JetMETObjects/data/Spring10_L3Absolute_AK5PF.txt");
   create("Spring10_AK5TRK","JetMETObjects/data/Spring10_L2Relative_AK5TRK.txt:JetMETObjects/data/Spring10_L3Absolute_AK5TRK.txt");
   create("Spring10_AK5JPT","JetMETObjects/data/Spring10_L2Relative_AK5JPT.txt:JetMETObjects/data/Spring10_L3Absolute_AK5JPT.txt");
+  create("Spring10_AK5CaloData","JetMETObjects/data/Spring10_L2Relative_AK5Calo.txt:JetMETObjects/data/Spring10_L3Absolute_AK5Calo.txt:JetMETObjects/data/Spring10DataV1_L2L3Residual_AK5Calo.txt"); 
+  create("Spring10_AK5PFData","JetMETObjects/data/Spring10_L2Relative_AK5PF.txt:JetMETObjects/data/Spring10_L3Absolute_AK5PF.txt:JetMETObjects/data/Spring10DataV1_L2L3Residual_AK5PF.txt"); 
+  create("Spring10_AK5JPTData","JetMETObjects/data/Spring10_L2Relative_AK5JPT.txt:JetMETObjects/data/Spring10_L3Absolute_AK5JPT.txt:JetMETObjects/data/Spring10DataV1_L2L3Residual_AK5JPT.txt");
 }
 
 JetMETCorFactorsFactory* JetMETCorFactorsFactory::Register::create(const std::string& name, const std::string& files) const
