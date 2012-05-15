59d104ecead7a7d2d9328675ff195b46fc495265
kirschen <>
2012-05-15 09:05:41 +0000

    symmetrise selection for JER-read in

--- DiJetReader.cc
+++ DiJetReader.cc
@@ -1,6 +1,6 @@
 //
 //    first version: Hartmut Stadie 2008/12/12
-//    $Id: DiJetReader.cc,v 1.86 2012/05/14 17:16:27 kirschen Exp $
+//    $Id: DiJetReader.cc,v 1.87 2012/05/14 17:20:15 kirschen Exp $
 //   
 #include "DiJetReader.h"
 
@@ -57,7 +57,7 @@ DiJetReader::DiJetReader(const std::string& configfile, Parameters* p)
   eventWeight_          = config_->read<double>("Di-Jet weight",-1.);
   weights_eq_one_       = config_->read<bool>("set weights to one",false);
   fire_all_dijet_triggers_    = config_->read<bool>("fire all triggers",false);
-  JERReadInJ1J2SameEtaBin_    = config_->read<bool>("JER - Assert J1J2 In Same Eta Bin",false);
+  JERReadInJ1J2SameEtaBin_    = config_->read<bool>("JER- Assert J1J2 In Same Eta Bin",false);
   std::cout << "JER - Assert J1J2 In Same Eta Bin set to: " << JERReadInJ1J2SameEtaBin_<<std::endl;
 
   useSingleJetTriggers_ = config_->read<bool>("Use single jet triggers",false);
@@ -541,6 +541,11 @@ int DiJetReader::readEventsFromTree(std::vector<Event*>& data)
 	  itjet2=JEREtaMap.lower_bound(std::abs(td->getJet2()->eta()));
 	  if(itjet1==itjet2){
 	    data.push_back(td); 
+	    ++nGoodEvts_;
+	    data.push_back(new TwoJetsPtBalanceEvent(td->getJet2()->clone(),td->getJet1()->clone(),
+						     td->getJet3() ? td->getJet3()->clone():0,
+						     td->ptHat(),td->weight(),td->nPU(),
+						     td->nPUTruth(),td->nVtx(),td->MET(),td->METphi(),td->runNumber()));
 	    ++nGoodEvts_;
 	  }
 	  //To do: Should the combination jet2,jet1 also be read in to be symmetric?
