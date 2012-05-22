d39ef9b7dd0e3046486f302059324b4e00df771a
kirschen <>
2012-05-22 07:52:31 +0000

    small script to set links in a targetdirectory pointing at multiple input n-tupels

---
+++ scripts/setLinksToMergeSamples.py
@@ -0,0 +1,54 @@
+#! /usr/bin/python
+
+import os
+import sys
+
+algo_types_all = ['ak5Calo_','ak5FastCalo_','ak5FastPF_','ak5JPT_','ak7PF_','ak7Calo_','ak5PFCHS_']
+
+length=len(sys.argv)
+samplelist=[]
+
+
+print "Use this script to set links to n-tuple files from multiple samples in a common targetdir."
+print "Usage: ./setlinks.py sample1 sample2 ... targetdir"
+print "------------------------------------------------------------------------------------------"
+print "number of arguments: " + str(length-1) + "; will set links to " + str(length-2) + " samples in targetdir"
+print "Warning: targetdir " + targetdir + "will be deleted before setting new links"
+
+for i in range(1,length):
+#	print str(i) + ": " + sys.argv[i]
+	samplelist.append(sys.argv[i])
+
+targetdir=samplelist[length-2]
+
+if len(sys.argv)<3:
+	print "error occured..."
+	print "usage: ./setlinks.py sample1 sample2 ... targetdir"
+	sys.exit()
+
+rep = ''
+while not rep in [ 'c', 'C' ]:
+	for j, sample in enumerate(samplelist):
+		if j<(length-2):
+			print "copying " + sample + " to " + targetdir
+	rep = raw_input( 'enter "c" to continue ("Ctrl+c" to abort): ' )
+	if 1 < len(rep):
+		rep = rep[0]
+
+
+print "targetdir: " +  targetdir
+
+if os.path.exists(targetdir):
+    os.system("rm -r " + targetdir)
+    os.system("mkdir " + targetdir)
+else:
+    os.system("mkdir " + targetdir)
+
+for algo_types in algo_types_all:
+	for j, sample in enumerate(samplelist):
+		if j<(length-2):
+			print "setting links to " + algo_types+ " " + sample + " in " + targetdir
+			for i in range(0,10):
+#				print i
+				os.system("ln -s " +sample +"/"+algo_types+str(i)+".root " + targetdir +"/"+algo_types+str(i)+"_sam"+str(j)+".root")
+
