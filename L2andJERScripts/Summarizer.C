8c833093081d9a063ff60fb3456098bdf2b8c07e
rathjd <>
2012-06-19 13:21:25 +0000

    Updated JEC summary readout, now in the right place

---
+++ L2andJERScripts/Summarizer.C
@@ -0,0 +1,102 @@
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TMath.h"
+#include "TF1.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TChain.h"
+#include "cmath" 
+#include "TPad.h"
+#include "TStyle.h"
+#include "TSpectrum.h"
+#include "TString.h"
+#include "TVector2.h"
+#include "TFile.h"
+#include "TVirtualFitter.h"
+#include "TLorentzVector.h"
+void Summarizer()
+{
+  bool verbose=false; //activate for debug mode
+  if(verbose)std::cout<<"start the summarizer"<<endl;
+  //some general settings
+  gROOT->Reset();
+  gROOT->SetStyle("Plain");
+  gStyle->SetOptStat(11111111);
+  gStyle->SetPalette(1);
+  //define alpha bins  
+  if(verbose)std::cout<<"setting binning"<<endl;
+  int alphaBin[4]={10, 15, 20, 30};
+  int etaBin[7]={0, 8, 13, 19, 25, 30, 52};
+  //loading the files  
+  if(verbose)std::cout<<"defining files"<<endl;
+  TFile *f;				      
+  TFile *save;
+  for(int type=0; type<2; type++)
+    {
+      if(verbose)std::cout<<"load type="<<type<<endl;  
+      if(type==0)f=TFile::Open("../20122012AB_195396_CORR2012SQLV7_AK5_MC_Su12Z2Star_PUS6S7_kostas_SQLV7AlternativeExtrapolAK5/dijetsFall10_TuneZ2_AK5PF_weighted_residuals_JEC_Mikko/plots/KalibriPlots.root");
+      else if(type==1)f=TFile::Open("../20122012AB_195396_CORR2012SQLV7_AK5_MC_Su12Z2Star_PUS6S7_kostas_SQLV7AlternativeExtrapolAK5/dijetsFall10_TuneZ2_AK5PFCHS_weighted_residuals_JEC_Mikko/plots/KalibriPlots.root");
+      if(verbose)std::cout<<"generate/update Summary.root"<<endl;   
+      //define write-target
+      if(type==0)save=new TFile("Summary.root", "recreate", "plots", 1);
+      if(type>0)save=new TFile("Summary.root", "update", "plots", 1);
+      //loop over RR/MPF method
+      for(int method=0; method<2; method++)
+	{
+	  if(verbose)std::cout<<"method loop="<<method<<endl; 
+	  //loop over alpha bins
+	  for(Int_t alpha=0; alpha<4; alpha++)
+	    {
+	      if(verbose)std::cout<<"alpha bin"<<alphaBin[alpha]<<endl; 
+	      //loop over eta bins
+	      for(Int_t eta=0; eta<6; eta++)
+		{
+		  if(verbose)std::cout<<"eta bin"<<etaBin[eta]<<endl; 		  
+		  //readout and write histograms
+		  TH1F *ReadData, *ReadMC;
+		  if(method==0)
+		    {
+		      //read histograms
+		      ReadData=(TH1F*)f->Get(TString::Format("AbsAsymmetryVsPt%d/AbsAsymmetryVsPt%d_AsymmetryVsMeanPt_data_L2L3_AbsEta%d_RatioOfMeans", alphaBin[alpha], alphaBin[alpha], eta));
+		      ReadData->GetYaxis()->SetRangeUser(0.95,1.05);
+		      ReadMC=(TH1F*)f->Get(TString::Format("AbsAsymmetryVsPt%d/AbsAsymmetryVsPt%d_AsymmetryVsMeanPt_MC_L2L3_AbsEta%d_RatioOfMeans", alphaBin[alpha], alphaBin[alpha], eta));
+		      ReadMC->GetYaxis()->SetRangeUser(0.95,1.05);
+		      //write histograms
+		      if(type==0)
+			{
+			  ReadData->Write(TString::Format("PtBal_a%d_eta%d_%d_data", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+			  ReadMC->Write(TString::Format("PtBal_a%d_eta%d_%d_MC", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));			  
+			}
+		      else if(type==1)
+			{
+			  ReadData->Write(TString::Format("PtBalchs_a%d_eta%d_%d_data", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+			  ReadMC->Write(TString::Format("PtBalchs_a%d_eta%d_%d_MC", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+
+			}
+		    }
+		  else if(method==1)
+		    {
+		      ReadData=(TH1F*)f->Get(TString::Format("AbsMPFVsPt%d/AbsMPFVsPt%d_MPFResponseVsJet2Pt_data_L2L3_AbsEta%d_Mean", alphaBin[alpha], alphaBin[alpha], eta));
+		      ReadMC=(TH1F*)f->Get(TString::Format("AbsMPFVsPt%d/AbsMPFVsPt%d_MPFResponseVsJet2Pt_MC_L2L3_AbsEta%d_Mean", alphaBin[alpha], alphaBin[alpha], eta));
+		      if(type==0)
+			{
+			  ReadData->Write(TString::Format("MPF_a%d_eta%d_%d_data", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+			  ReadMC->Write(TString::Format("MPF_a%d_eta%d_%d_MC", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+
+			}
+		      else if(type==1)
+			{
+			  ReadData->Write(TString::Format("MPFchs_a%d_eta%d_%d_data", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+			  ReadMC->Write(TString::Format("MPFchs_a%d_eta%d_%d_MC", alphaBin[alpha], etaBin[eta], etaBin[eta+1]));
+			}
+		    }
+		  if(verbose)std::cout<<"finished one iteration"<<endl; 
+		}
+	    }
+	}
+      save->Close();
+      f->Close();
+    }
+}
