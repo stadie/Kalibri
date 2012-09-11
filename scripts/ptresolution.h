424fe5ac5e5fff6d632f04b8036d7da708b30811
kirschen <>
2012-09-11 12:56:19 +0000

    simplified version to only use scaling factors, not the full pt-resolution parametrisation; implemented smearing of MET; using old JME and Matthias Summer 11 numbers (official recommendation)

--- scripts/ptresolution.h
+++ scripts/ptresolution.h
@@ -6,137 +6,80 @@
 
 #include "TMath.h"
 
-// Switch MC truth or data resolutions
-bool _ismcjer = true;
-bool _jerkscale = false;
 
 // Hauke's resolutions
-const int _nres = 7;//6;
-const int _npar = 4;
-/* float _pres[_nres][_npar] = */
-/* // From CMSSW Fall10_PtResolution_AK5PF (Feb11-Feb25) */
-/*   {{/\*0 0.5*\/ 3.96859, 0.183476, 0, 0.626273},// 0.026724 */
-/*    {/\*0.5 1*\/ 3.55226, 0.240261, 0, 0.525708},// 0.0254424 */
-/*    {/\*1 1.5*\/ 4.54826, 0.226521, 0, 0.589632},// 0.0335984 */
-/*    {/\*1.5 2*\/ 4.62622, 0.236643, 0, 0.487379},// 0.0448822 */
-/*    {/\*2 2.5*\/ 2.53324, 0.343062, 0, 0.286621},// 0.0517279 */
-/*    {/\*2.5 3*\/-3.33814, 0.733604, 0, 0.0826378},// 0.070123 */
-/*    {/\*3 9.9*\/ 2.95397, 0.116194, 0, 0.960861}};// 0.0564672 */
-/* float _pres_ak5jpt[_nres][_npar] = */
-/* // From CMSSW Fall10_PtResolution_AK5JPT (Feb11-Feb25) */
-/*   {{/\*0 0.5*\/ 4.76130, 0.224764, 0, 0.546172},// 0.039651 */
-/*    {/\*0.5 1*\/ 4.49601, 0.262162, 0, 0.496672},// 0.0188774 */
-/*    {/\*1 1.5*\/ 4.91436, 0.298285, 0, 0.497331},// 0.034662 */
-/*    {/\*1.5 2*\/ 5.35503, 0.310884, 0, 0.415469},// 0.0416232 */
-/*    {/\*2 2.5*\/ 3.96073, 0.411402, 0, 0.362306},// 0.0502304 */
-/*    {/\*2.5 3*\/ 0.718871, 0.47073, 0, 0.213428},// 0.0616422 */
-/*    {/\*3 9.9*\/ 2.95397, 0.116194, 0, 0.960861}};// 0.0564672 (from PF) */
-/* float _pres_ak5calo[_nres][_npar] = */
-/* // From CMSSW Fall10_PtResolution_AK5CALO (Feb11-Feb25) */
-/*   {{/\*0 0.5*\/ 5.89372, 0.398184, 0, 0.411555},// 0.0325261 */
-/*    {/\*0.5 1*\/ 4.70682, 0.586653, 0, 0.277176},// 0.0512329 */
-/*    {/\*1 1.5*\/ 5.06249, 0.575817, 0, 0.317870},// 0.0392882 */
-/*    {/\*1.5 2*\/ 3.86101, 0.805593, 0, 0.163246},// 0.0673781 */
-/*    {/\*2 2.5*\/ 3.05359, 0.487558, 0, 0.243042},// 0.0453157 */
-/*    {/\*2.5 3*\/-1.91901, 0.597099, 0, 0.127347},// 0.0708867 */
-/*    {/\*3 9.9*\/ 2.81913, 0.0881245, 0, 1.11217}};// 0.0864411 */
+int _nres = 6;
 
-float _pres[_nres][_npar] =
- // From CMSSW Spring10_PtResolution_AK5PF
- {{/*0 0.5*/ -0.349206, 0.297831, 0, 0.471121},
-  {/*0.5 1*/ -0.499735, 0.336391, 0, 0.430689},
-  {/*1 1.5*/ -0.561649, 0.420293, 0, 0.392398},
-  {/*1.5 2*/ -1.12329,  0.657891, 0, 0.139595},
-  {/*2 2.5*/  1.04792,  0.466763, 0, 0.193137},
-  {/*2.5 3*/  2.56933,  0.305802, 0, 0.398929}, // changed since May5?
-  {/*2.5 3*/  2.56933,  0.305802, 0, 0.398929}}; // copy
-  // Recover old fit for test purposes
-  //{ 1.89978, 0.33427, 0.00000, 0.36547}};// 2.5-3.0 (28.3/9), May 5 evening
-float _pres_ak5jpt[_nres][_npar] =
- // From CMSSW Spring10_PtResolution_AK5JPT
- {{/*0 0.5*/ 2.51562, 0.316061, 0, 0.445628},
-  {/*0.5 1*/ 2.41271, 0.332129, 0, 0.432430},
-  {/*1 1.5*/ 2.42631, 0.371536, 0, 0.439477},
-  {/*1.5 2*/ 2.67972, 0.511479, 0, 0.257115},
-  {/*2 2.5*/ 3.39005, 0.499497, 0, 0.273727},
-  {/*2.5 3*/ 2.28498, 0.390866, 0, 0.297617},
-  {/*2.5 3*/ 2.28498, 0.390866, 0, 0.297617}}; // copy
-float _pres_ak5calo[_nres][_npar] =
- // From CMSSW Spring10_PtResolution_AK5Calo
- {{/*0 0.5*/ 5.0913, 0.511619, 0, 0.325477},
-  {/*0.5 1*/ 4.93688, 0.543349, 0, 0.310638},
-  {/*1 1.5*/ 4.95729, 0.574202, 0, 0.32344},
-  {/*1.5 2*/ 3.37322, 0.96262, 0, 0.0860736},
-  {/*2 2.5*/ 4.06368, 0.351561, 0, 0.356167},
-  {/*2.5 3*/ 2.59292, 0.357679, 0, 0.321092},
-  {/*2.5 3*/ 2.59292, 0.357679, 0, 0.321092}}; // copy
+float Matthias_eta_bins [] =  {0, 0.5, 1.1, 1.7, 2.3, 5.2};
+float JME_eta_bins [] =  {0, 1.1, 1.7, 2.3, 5.2};
 
-// Hauke's numbers, Feb 11; confirmed Feb25 (in Fall10_PtResolution_AK5*.txt)
-float _pconst_ak5pf[_nres] =   {0.027, 0.025, 0.034, 0.045, 0.052, 0.070,0.056};
-float _pconst_ak5calo[_nres] = {0.033, 0.051, 0.039, 0.067, 0.045, 0.071,0.086};
-float _pconst_ak5jpt[_nres] =  {0.040, 0.019, 0.035, 0.042, 0.050, 0.062,0.062};
-// Replace 1.5<|y|<3. numbers with my own for PF to improve ansatz fit (Feb23)
-//float _pconst_ak5pf[_nres] =   {0.034, 0.030, 0.032, 0.029, 0.026, 0.043};
-//float _pconst_ak5calo[_nres] = {0.033, 0.051, 0.039, 0.067, 0.045, 0.043};
-//float _pconst_ak5jpt[_nres] =  {0.040, 0.019, 0.035, 0.042, 0.026, 0.043};
-// Replace 1.5<|y|<3. numbers with my own for PF to improve ansatz fit (Feb24)
-//float _pconst_ak5pf[_nres] =   {0.037, 0.032, 0.034, 0.028, 0.027, 0.059};
-//float _pconst_ak5calo[_nres] = {0.033, 0.051, 0.039, 0.067, 0.045, 0.059};
-//float _pconst_ak5jpt[_nres] =  {0.040, 0.019, 0.035, 0.042, 0.027, 0.059};
 
-// k-scale for PFJets from JME-10-014
-float _pscale_ak5pf[_nres] = {1.07, 1.07, 1.10, 1.10, 1.07, 1.18};
 
-double ptresolution(float pt, float eta) {
-  
-  int ieta = min(_nres-1, int(fabs(eta) / 0.5));
-  float N = _pres[ieta][0];
-  float S = _pres[ieta][1];
-  float C = _pres[ieta][2];
-  float m = _pres[ieta][3];
-  double c = _pconst_ak5pf[ieta];
-  double k = _pscale_ak5pf[ieta];
-  if (_ismcjer || !_jerkscale) k = 1.;
-  if (_ismcjer ||  _jerkscale) c = 0.;
+float *pointerToEtaBins;
+float *pointerToScaleFactors;
 
-  double res = k*sqrt(TMath::Sign(N*N,N)/(pt*pt) + S*S*pow(pt,m-1) + C*C + c*c);
-  //if (ieta==0 && res>0.12) res = 0.12;
-
-  return res;
+int find_ieta(float eta){
+  int ieta=-1;
+  for(int i=0;i<_nres-1;i++){
+    if(std::abs(eta)>=pointerToEtaBins[i]&&std::abs(eta)<pointerToEtaBins[i+1])ieta=i;
+  }
+  return ieta;
 }
 
-double ptresolution_jpt(float pt, float eta) {
+//// k-scale for PFJets from Matthias, July 14, Hamburg Calib
+float _pscale_ak5pf_M[] = {1.052, 1.057, 1.096, 1.134, 1.288};
+
+// downward fluctuation values
+float _pscale_ak5pf_M_d[] =
+{
+1.052-TMath::Sqrt(pow(0.012,2)+pow(0.061,2)),
+1.057-TMath::Sqrt(pow(0.012,2)+pow(0.055,2)), 
+1.096-TMath::Sqrt(pow(0.017,2)+pow(0.062,2)), 
+1.134-TMath::Sqrt(pow(0.035,2)+pow(0.085,2)), 
+1.288-TMath::Sqrt(pow(0.127,2)+pow(0.153,2))};
+
+// upward fluctuation values
+float _pscale_ak5pf_M_u[] =
+{
+1.052+TMath::Sqrt(pow(0.012,2)+pow(0.061,2)),
+1.057+TMath::Sqrt(pow(0.012,2)+pow(0.055,2)), 
+1.096+TMath::Sqrt(pow(0.017,2)+pow(0.062,2)), 
+1.134+TMath::Sqrt(pow(0.035,2)+pow(0.085,2)), 
+1.288+TMath::Sqrt(pow(0.127,2)+pow(0.153,2))};
+
+
+  //according to JME-10-14 table 1
+float _pscale_ak5calo_M_JME[] ={ 1.079, 1.054, 1.061, 1.174};//Calo
+
+
+void configureSmearfactor(TString chooseScaleFactors) {
   
-  int ieta = min(_nres-1, int(fabs(eta) / 0.5));
-  float N = _pres_ak5jpt[ieta][0];
-  float S = _pres_ak5jpt[ieta][1];
-  float C = _pres_ak5jpt[ieta][2];
-  float m = _pres_ak5jpt[ieta][3];
-  double c = _pconst_ak5jpt[ieta];
-  double k = 1.;
-  if (_ismcjer) k = 1.;
-  if (_ismcjer) c = 0.;
 
-  double res = k*sqrt(TMath::Sign(N*N,N)/(pt*pt) + S*S*pow(pt,m-1) + C*C + c*c);
+  if(chooseScaleFactors.Contains("Matthias")){
+    pointerToEtaBins=Matthias_eta_bins;
+    _nres=6;
+  }
+  else if(chooseScaleFactors.Contains("JME")){
+    pointerToEtaBins=JME_eta_bins;
+    _nres=5;
+  }
+  if(chooseScaleFactors=="PF_Matthias")pointerToScaleFactors=_pscale_ak5pf_M;
+  if(chooseScaleFactors=="PF_Matthias_u")pointerToScaleFactors=_pscale_ak5pf_M_u;
+  if(chooseScaleFactors=="PF_Matthias_d")pointerToScaleFactors=_pscale_ak5pf_M_d;
 
-  return res;
+  if(chooseScaleFactors=="Calo_JME")pointerToScaleFactors=_pscale_ak5calo_M_JME;
+
+  std::cout << "Using " << _nres-1 << " bins configured via " << chooseScaleFactors << " for smearing."<< std::endl;
+  std::cout << "Scalefactors: "<< std::endl;
+  for(int i =0;i<_nres-1;i++)std::cout << pointerToEtaBins[i] <<"<"<<pointerToEtaBins[i+1] <<": \t" << pointerToScaleFactors[i] <<std::endl;
 }
 
-double ptresolution_calo(float pt, float eta) {
+double getScaleFactor(float eta){
+  int ieta = find_ieta(eta);
+  return pointerToScaleFactors[ieta];
   
-  int ieta = min(_nres-1, int(fabs(eta) / 0.5));
-  float N = _pres_ak5calo[ieta][0];
-  float S = _pres_ak5calo[ieta][1];
-  float C = _pres_ak5calo[ieta][2];
-  float m = _pres_ak5calo[ieta][3];
-  double c = _pconst_ak5calo[ieta];
-  double k = 1.;
-  if (_ismcjer) k = 1.;
-  if (_ismcjer) c = 0.;
-
-  double res = k*sqrt(TMath::Sign(N*N,N)/(pt*pt) + S*S*pow(pt,m-1) + C*C + c*c);
-
-  return res;
 }
+
+
 
 #endif // __ptresolution_h__
--- scripts/smearJets.C
+++ scripts/smearJets.C
@@ -1,15 +1,17 @@
-// $Id: smearJets.C,v 1.3 2011/02/04 10:35:44 mschrode Exp $
+// $Id: smearJets.C,v 1.1 2011/04/07 08:04:58 stadie Exp $
 
 #include "TChain.h"
+#include "TString.h"
 #include "TBranch.h"
 #include "TFile.h"
+#include <TVector2.h>
 
 #include <cmath>
 #include <iostream>
 
 #include "scripts/ptresolution.h"
 
-void smearJetsInFile(const char* path,const char* outpath, int smeartype) {
+void smearJetsInFile(const char* path,const char* outpath) {
   Int_t           NobjGenJet;
   Float_t         GenJetColPt[100];   //[NobjGenJet]
   Int_t           GenJetColJetIdx[100];   //[NobjGenJet]
@@ -17,18 +19,20 @@ void smearJetsInFile(const char* path,const char* outpath, int smeartype) {
   Float_t         JetPtold[100];   //[NobjJet]
   Float_t         JetEtold[100];   //[NobjJet]
   Float_t         JetEold[100];   //[NobjJet]
+  Float_t         JetPhiold[100];   //[NobjJet]
   Float_t         JetPt[100];   //[NobjJet]
   Float_t         JetEt[100];   //[NobjJet]
   Float_t         JetEta[100];   //[NobjJet]
-  Float_t         JetE[100];   //[NobjJet]
+  Float_t         JetE[100];   //[NobjJet] 
   Float_t         JetCorrL2L3[100];   //[NobjJet]
-  //according to JME-10-14 table 1
-  float smearFactors[4][4] = { 
-    { 1.079, 1.054, 1.061, 1.174},//Calo
-    { 1.059, 1.115, 0.993, 1.062},//JPT
-    { 1.04,  1.02,  0.91,  1.04 },//PF
-    { 1.07,  1.10,  1.07,  1.18 } //PF from Gamma
-  };
+  Float_t         JetCorrL1[100];   //[NobjJet]
+  Float_t         MetOld;
+  Float_t         MetPhiOld;
+  Float_t         Met;
+  Float_t         MetPhi;
+
+  Bool_t do_MET_smearing=true;
+  Int_t count_nans_for_MET_smearing=1;
 
   TFile *fin = TFile::Open(path);
   TTree *tin = (TTree*)fin->Get("DiJetTree");
@@ -37,8 +41,12 @@ void smearJetsInFile(const char* path,const char* outpath, int smeartype) {
   tin->SetBranchStatus("JetPt",0);
   tin->SetBranchStatus("JetEt",0);
   tin->SetBranchStatus("JetE",0);
+  tin->SetBranchStatus("Met",0); 
+  tin->SetBranchStatus("MetPhi",0); 
 
   Long64_t nentries = tin->GetEntries();
+
+  //  nentries=1000;
 
   TFile *fout = TFile::Open(outpath,"UPDATE");
   TTree *tout =  tin->CloneTree(nentries,"fast");
@@ -50,97 +58,164 @@ void smearJetsInFile(const char* path,const char* outpath, int smeartype) {
   tin->SetBranchStatus("JetEt",1);
   tin->SetBranchStatus("JetEta",1);
   tin->SetBranchStatus("JetE",1);
+  tin->SetBranchStatus("JetPhi",1);
   tin->SetBranchStatus("NobjGenJet",1);
   tin->SetBranchStatus("GenJetColPt",1);
   tin->SetBranchStatus("GenJetColJetIdx",1); 
   tin->SetBranchStatus("JetCorrL2L3",1); 
+  tin->SetBranchStatus("JetCorrL1",1); 
+  tin->SetBranchStatus("Met",1); 
+  tin->SetBranchStatus("MetPhi",1); 
 
   tin->SetBranchAddress("NobjJet", &NobjJet);
   tin->SetBranchAddress("JetPt", JetPtold);
   tin->SetBranchAddress("JetEt", JetEtold);
   tin->SetBranchAddress("JetEta", JetEta);
   tin->SetBranchAddress("JetE", JetEold);
+  tin->SetBranchAddress("JetPhi", JetPhiold);
   tin->SetBranchAddress("NobjGenJet", &NobjGenJet);
   tin->SetBranchAddress("GenJetColPt", GenJetColPt);
   tin->SetBranchAddress("GenJetColJetIdx", GenJetColJetIdx);
   tin->SetBranchAddress("JetCorrL2L3", JetCorrL2L3);
+  tin->SetBranchAddress("JetCorrL1", JetCorrL1);
+  tin->SetBranchAddress("Met", &MetOld);
+  tin->SetBranchAddress("MetPhi", &MetPhiOld);
 
   
-  TBranch* b_JetPt = tout->Branch("JetPt",JetPt, "JetPt[NobjJet]/F");
-  TBranch* b_JetEt = tout->Branch("JetEt",JetEt, "JetEt[NobjJet]/F");
-  TBranch* b_JetE  = tout->Branch("JetE",JetE, "JetE[NobjJet]/F");
+  TBranch* b_JetPt  = tout->Branch("JetPt",JetPt, "JetPt[NobjJet]/F");
+  TBranch* b_JetEt  = tout->Branch("JetEt",JetEt, "JetEt[NobjJet]/F");
+  TBranch* b_JetE   = tout->Branch("JetE",JetE, "JetE[NobjJet]/F");
+  TBranch* b_Met    = tout->Branch("Met",&Met, "Met/F");
+  TBranch* b_MetPhi = tout->Branch("MetPhi",&MetPhi, "MetPhi/F");
   //tout->SetBranchStatus("*",1);
 
   for (Long64_t i = 0; i < nentries; ++i) {
+    if(i % 100000 == 0)printf("event %d\n", i); //Alle tausend Events was sagen
     tin->GetEntry(i);
     for(int j = 0 ; j <  NobjJet ; ++j) {
       JetPt[j] = JetPtold[j];
       JetEt[j] = JetPtold[j];
       JetE[j] = JetPtold[j];
     }
+    Met=MetOld;
+    MetPhi=MetPhiOld;
+    TVector2* MET = new TVector2(1,1);
+    MET->SetMagPhi(Met,MetPhi);
+    //    cout << "MAG: " << Met << " phi: " << MetPhi << endl;
     for(int j = NobjGenJet -1  ; j >= 0 ; --j) {
       int id = GenJetColJetIdx[j];
       if((id >=  NobjJet)|| (id < 0)) continue;
-      float diff = JetCorrL2L3[id] * JetPtold[id] - GenJetColPt[j];
-      float aeta = std::abs(JetEta[id]);
-      int ieta = 3;
-      if (aeta < 1.1) ieta = 0;
-      else if (aeta < 1.7) ieta = 1;
-      else if (aeta < 2.3) ieta = 2;
+      //do not let JetCorrL1 be negative...
+      float diff = max(JetCorrL1[id],(Float_t)0.0001) * JetCorrL2L3[id] * JetPtold[id] - GenJetColPt[j];
       float smearfactor = 1.0;
-      if(smeartype < 4) smearfactor = smearFactors[smeartype][ieta];
-      else {
-	if(smeartype == 4) {
-	  _ismcjer = true;
-	  float resmc = ptresolution_calo(GenJetColPt[j],JetEta[id]);
-	  _ismcjer = false;
-	  float resdata = ptresolution_calo(GenJetColPt[j],JetEta[id]);
-	  smearfactor = resdata/resmc;	
-	}
-	if(smeartype == 5) {
-	  _ismcjer = true;
-	  float resmc = ptresolution_jpt(GenJetColPt[j],JetEta[id]);
-	  _ismcjer = false;
-	  float resdata = ptresolution_jpt(GenJetColPt[j],JetEta[id]);
-	  smearfactor = resdata/resmc;
-	}
-	if(smeartype == 6) {
-	  _ismcjer = true;
-	  float resmc = ptresolution(GenJetColPt[j],JetEta[id]);
-	  _ismcjer = false;
-	  float resdata = ptresolution(GenJetColPt[j],JetEta[id]);
-	  smearfactor = resdata/resmc;
-	}
+      smearfactor = getScaleFactor(JetEta[id]);
+      if(TMath::IsNaN(smearfactor)){
+	cout << "DEBUG... GenJetColPt[j]: " << GenJetColPt[j] << " JetEta[id]: " << JetEta[id] <<  endl;
       }
-      //std::cout << GenJetColPt[j] << ", " << JetEta[id] << ": " <<  smearfactor
-      //		<< '\n';
-      JetPt[id] =  JetPtold[id] + (smearfactor - 1.0) * diff /JetCorrL2L3[id];
+//      if(i % 100000 == 0){
+//	printf("event %d\n", i); //Alle tausend Events was sagen
+//	cout << smearfactor << endl;
+//	cout << "DEBUG... GenJetColPt[j]: " << GenJetColPt[j] << " JetEta[id]: " << JetEta[id] <<  endl;
+//      }
+
+      // let L1JetCorr not be negative and make sure JetPt is above
+      // some threshold
+      if(GenJetColPt[j]>10.){
+	JetPt[id] =  JetPtold[id] + (smearfactor - 1.0) * diff / (max(JetCorrL1[id],(Float_t)0.0001) * JetCorrL2L3[id]);
+      }
       float k =  JetPt[id] /  JetPtold[id];
       //float k =  (1-smearfactor) * GenJetColPt[j]/JetPtold[id] + smearfactor;
       //JetPt[id] = k * JetPtold[id];
       JetEt[id] = k * JetEtold[id];
       JetE[id] = k * JetEold[id];
+      if(do_MET_smearing){
+	TVector2* MET_alteration = new TVector2(1,1);
+	TVector2* pt_old = new TVector2(1,1);
+	TVector2* pt_smeared = new TVector2(1,1);
+	
+	pt_old->SetMagPhi(JetPtold[id],JetPhiold[id]);
+	pt_smeared->SetMagPhi(JetPt[id],JetPhiold[id]);
+	*MET_alteration= *pt_old-*pt_smeared;
+	//    MET_alteration->Print();
+	//    MET->Print();
+	if(TMath::IsNaN(MET_alteration->Mod())){
+	  count_nans_for_MET_smearing++;
+	  cout << "DEBUG... Ptold: " << JetPtold[id] << " JetPt: " << JetPt[id]<< " JetPhi: " << JetPhiold[id]  << " smearfactor: " << smearfactor << " JetCorrL2L3[id]: " << JetCorrL2L3[id] << " JetCorrL1[id]: " << JetCorrL1[id] << " MET-alteration failed: " << count_nans_for_MET_smearing << endl;
+	}
+	else{
+	  *MET+=*MET_alteration;
+	}
+	//    MET->Print();
+	MET_alteration->Delete();
+	pt_old->Delete();
+	pt_smeared->Delete();
+      }
     }
+    //        cout << "BEFORE... MAG: " << Met << " phi: " << MetPhi << endl;
+    Met=MET->Mod();
+    MetPhi=MET->Phi_mpi_pi(MET->Phi());
+    //    cout << "AFTER.... MAG: " << Met << " phi: " << MetPhi << endl;
+
     b_JetPt->Fill();
     b_JetEt->Fill();
     b_JetE->Fill();
+    b_Met->Fill();
+    b_MetPhi->Fill();
   }
   fin->Close();
   tout->Write("", TObject::kOverwrite);
   fout->Close(); 
+  cout << "MET-alterations failing due to NANs: " << count_nans_for_MET_smearing << endl;
+
 }
 
+
 void smearJets() {
-  TString sin("/scratch/hh/current/cms/user/stadie/QCD_Pt_15to3000_TuneZ2_Flat_7TeV_pythia6_Fall10-START38_V12-v1Dmerged/ak5Calo");
-  TString son("/scratch/hh/current/cms/user/stadie/QCD_Pt_15to3000_TuneZ2_Flat_7TeV_pythia6_Fall10-START38_V12-v1Dsmeared3/ak5Calo");
-  for(int i = 0 ; i < 10 ; ++i) {
-    TString sif = sin + "_";
-    sif += i;
-    sif +=".root";
-    std::cout << "smearing " << sif << '\n';
-    TString sof = son + "_";
-    sof += i;
-    sof += ".root";
-    smearJetsInFile(sif,sof,4);
+  std::vector<TString> sInPath;
+  std::vector<TString> sOutPath;
+  std::vector<TString> scaleFactors;
+
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5Calo");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared/ak5Calo");
+  scaleFactors.push_back("Calo_JME");
+
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5FastPF");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared/ak5FastPF");
+  scaleFactors.push_back("PF_Matthias");
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5PFCHS");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared/ak5PFCHS");
+  scaleFactors.push_back("PF_Matthias");
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5FastPF");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared_u/ak5FastPF");
+  scaleFactors.push_back("PF_Matthias_u");
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5PFCHS");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared_u/ak5PFCHS");
+  scaleFactors.push_back("PF_Matthias_u");
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5FastPF");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared_d/ak5FastPF");
+  scaleFactors.push_back("PF_Matthias");
+  sInPath.push_back("/scratch/hh/current/cms/user/rathjd/Calibration/MCSummer12S10DX53/ak5PFCHS");
+  sOutPath.push_back("/scratch/hh/current/cms/user/kirschen/2012_Jets_v6/MCSummer12S10DX53_Smeared_d/ak5PFCHS");
+  scaleFactors.push_back("PF_Matthias");
+
+
+
+
+  assert(sInPath.size()==sOutPath.size());
+  assert(sInPath.size()==scaleFactors.size());
+
+
+  for(unsigned int sample_i = 0 ; sample_i < sOutPath.size() ; ++sample_i) {
+    configureSmearfactor(scaleFactors.at(sample_i));
+    for(int i = 0 ; i < 10 ; ++i) {
+      TString sif = sInPath.at(sample_i)  + "_";
+      sif += i;
+      sif +="_sam0.root";
+      std::cout << "smearing " << sif << '\n';
+      TString sof = sOutPath.at(sample_i) + "_";
+      sof += i;
+      sof += "sam0.root";
+      smearJetsInFile(sif,sof);
+    }
   }
 }
