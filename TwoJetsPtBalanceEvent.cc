2ee6ccbfa71f3181dddcb44d1a22b61fe3dde3c5
rathjd <>
2012-10-04 13:18:01 +0000

    changed SJ variable back to lead jet

--- TwoJetsPtBalanceEvent.cc
+++ TwoJetsPtBalanceEvent.cc
@@ -1,4 +1,4 @@
-// $Id: TwoJetsPtBalanceEvent.cc,v 1.13 2012/09/11 10:06:05 kirschen Exp $
+// $Id: TwoJetsPtBalanceEvent.cc,v 1.14 2012/09/12 12:24:50 kirschen Exp $
 
 #include "TwoJetsPtBalanceEvent.h"
 #include "TVector2.h"
@@ -489,7 +489,14 @@ double TwoJetsPtBalanceEvent::relPtJet3ProjectionCorrL2L3() const {
 double TwoJetsPtBalanceEvent::triggerPtVariableL2L3(bool useSingleJetTriggers) const{
  
   if(!useSingleJetTriggers)return ptDijetCorrL2L3();
-  else return jet2_->corFactors().getL2L3() * jet2_->pt();
+  else{
+    double ptcorj1,ptcorj2; 	
+    ptcorj1 = jet1_->corFactors().getL2L3() * jet1_->pt(); 	 
+    ptcorj2 = jet2_->corFactors().getL2L3() * jet2_->pt(); 	 
+    if(ptcorj1>ptcorj2)return ptcorj1; 	 
+    else return ptcorj2; 	 
+      }
+	   //return jet2_->corFactors().getL2L3() * jet2_->pt(); //barrel jet version, lowers scale
 
 
 }
