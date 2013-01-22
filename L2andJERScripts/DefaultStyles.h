a110a82d3117a3883a66bae7ac7770a6f7348da6
kirschen <>
2013-01-22 11:00:55 +0000

    add range-check to catch too many entries

--- L2andJERScripts/DefaultStyles.h
+++ L2andJERScripts/DefaultStyles.h
@@ -7,8 +7,16 @@ class DefaultStyles {
  public:
 
   DefaultStyles();
-  int getColor(int color_i) {return colors_.at(color_i);}
-  int getMarker(int marker_i) {return markers_.at(marker_i);}
+  int getColor(int color_i) {
+    if(color_i>colors_.size())std::cout << "color index out of range" <<std::endl;
+    assert(color_i<colors_.size());
+    return colors_.at(color_i);
+  }
+  int getMarker(int marker_i) {
+    if(marker_i>markers_.size())std::cout << "marker index out of range" <<std::endl;
+    assert(marker_i<markers_.size());
+    return markers_.at(marker_i);
+}
   void setStyle(TString stylelabel = "PFComp");
   
  private:
