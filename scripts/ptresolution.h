7772115cc011d71954101b0757899d41d72d963d
Denis Rathjens <Denis.Rathjens@cern.ch>
2014-06-03 16:35:24 +0200

    included forward extension JER values from Kristin

--- scripts/ptresolution.h
+++ scripts/ptresolution.h
@@ -13,6 +13,7 @@ int _nres = 6;
 
 float Matthias_eta_bins [] =  {0, 0.5, 1.1, 1.7, 2.3, 5.2};
 float JME_eta_bins [] =  {0, 1.1, 1.7, 2.3, 5.2}; //jinst
+float Kristin_eta_bins [] = {0,0.5,1.1,1.7,2.3,2.8,3.2,5.2};
 
 
 
@@ -57,6 +58,15 @@ float _pscale_ak5pf_K_d[] = {1.077-0.026, 1.100-0.028, 1.119-0.029, 1.205-0.045,
 //upward variation values
 float _pscale_ak5pf_K_u[] = {1.077+0.026, 1.100+0.028, 1.119+0.029, 1.205+0.045, 1.191+0.079};
 
+//k-scale for PFjets from Kristin with forward extension, June 03 2014
+float _pscale_ak5pf_Kfe[] = {1.079, 1.099, 1.121, 1.208, 1.254, 1.395, 1.056};
+
+//downward variation values
+float _pscale_ak5pf_Kfe_d[] = {1.079-0.026, 1.099-0.028, 1.121-0.029, 1.208-0.046, 1.254-0.062, 1.395-0.063, 1.056-0.191};
+
+//upward variation values
+float _pscale_ak5pf_Kfe_u[] = {1.079+0.026, 1.099+0.028, 1.121+0.029, 1.208+0.046, 1.254+0.062, 1.395+0.063, 1.056+0.191};
+
 
   //according to JINST paper/JER twiki
 float _pscale_ak5calo_M_JME[] ={ 1.088, 1.139, 1.082, 1.065};//Calo
@@ -76,6 +86,10 @@ void configureSmearfactor(TString chooseScaleFactors) {
     pointerToEtaBins=Matthias_eta_bins;
     _nres=6;
   }
+  else if(chooseScaleFactors.Contains("KristinFE")){
+    pointerToEtaBins=Kristin_eta_bins;
+    _nres=8;
+  }
   else if(chooseScaleFactors.Contains("JME")){
     pointerToEtaBins=JME_eta_bins;
     _nres=5; 
@@ -89,6 +103,10 @@ void configureSmearfactor(TString chooseScaleFactors) {
   else if(chooseScaleFactors=="PF_Kristin")pointerToScaleFactors=_pscale_ak5pf_K;
   else if(chooseScaleFactors=="PF_Kristin_u")pointerToScaleFactors=_pscale_ak5pf_K_u;
   else if(chooseScaleFactors=="PF_Kristin_d")pointerToScaleFactors=_pscale_ak5pf_K_d;
+  
+  else if(chooseScaleFactors=="PF_KristinFE")pointerToScaleFactors=_pscale_ak5pf_Kfe;
+  else if(chooseScaleFactors=="PF_KristinFE_u")pointerToScaleFactors=_pscale_ak5pf_Kfe_u;
+  else if(chooseScaleFactors=="PF_KristinFE_d")pointerToScaleFactors=_pscale_ak5pf_Kfe_d;
 
   else if(chooseScaleFactors=="Calo_JME")pointerToScaleFactors=_pscale_ak5calo_M_JME;
   else if(chooseScaleFactors=="JPT_JME")pointerToScaleFactors=_pscale_ak5calo_M_JME;
